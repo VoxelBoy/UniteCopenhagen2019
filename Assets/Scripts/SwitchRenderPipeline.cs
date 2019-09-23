0d75a00b9fb396eba6de0c84329982b22b90aba4
Yilmaz Kiymaz <yilmaz.kiymaz@innogames.com>
2019-09-23 10:40:53 +0200

    Fix compile error in SwitchRenderPipeline class when UniversalRP package is not installed

--- Assets/Scripts/SwitchRenderPipeline.cs
+++ Assets/Scripts/SwitchRenderPipeline.cs
@@ -2,7 +2,6 @@
 using UnityEditor.ShortcutManagement;
 using UnityEngine;
 using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
 
 public class SwitchRenderPipeline
 {
@@ -10,7 +9,7 @@ public class SwitchRenderPipeline
     [Shortcut("SwitchRenderPipeline", null, KeyCode.R, ShortcutModifiers.Alt | ShortcutModifiers.Shift)]
     private static void DoSwitchRenderPipeline()
     {
-        var renderPipelineAsset = AssetDatabase.LoadAssetAtPath<UniversalRenderPipelineAsset>("Assets/UniversalRenderPipelineAsset.asset");
+        var renderPipelineAsset = AssetDatabase.LoadAssetAtPath<RenderPipelineAsset>("Assets/UniversalRenderPipelineAsset.asset");
         GraphicsSettings.renderPipelineAsset = GraphicsSettings.renderPipelineAsset != null ? null : renderPipelineAsset;
         var activeRenderPipeline = GraphicsSettings.renderPipelineAsset != null ? "URP" : "Built-in RP";
         SceneView.lastActiveSceneView.ShowNotification(new GUIContent($"Switched to {activeRenderPipeline}"));
