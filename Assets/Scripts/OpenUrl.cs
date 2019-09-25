331c8ee54397caf0e45f1d11b9dee1c0a1eb5f7f
Yilmaz Kiymaz <yilmaz.kiymaz@innogames.com>
2019-09-25 11:25:17 +0200

    OpenUrl fixes

--- Assets/Scripts/OpenUrl.cs
+++ Assets/Scripts/OpenUrl.cs
@@ -1,9 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Button))]
 public class OpenUrl : MonoBehaviour
 {
 
 	public string url;
+
+	public void Awake()
+	{
+		GetComponent<Button>().onClick.AddListener(OnButtonClicked);
+	}
 
 	public void OnButtonClicked()
 	{
