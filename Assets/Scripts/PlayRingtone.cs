7854ea2fde0eabe3f318ff995c4a961f8f5aea30
Yilmaz Kiymaz <yilmaz.kiymaz@innogames.com>
2019-09-25 07:58:18 +0200

    Lots of work on slides

---
+++ Assets/Scripts/PlayRingtone.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayRingtone : MonoBehaviour
+{
+
+    [SerializeField] private AudioSource source;
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (source.isPlaying)
+            {
+                source.Stop();
+            }
+            else
+            {
+                source.Play();
+            }
+        }
+    }
+}
